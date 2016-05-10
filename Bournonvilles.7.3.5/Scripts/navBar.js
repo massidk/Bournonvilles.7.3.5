9f7438689a9ac7d1dcb4a017376defe4662c7ed3
massidk <e-david@live.dk>
2016-05-10 09:00:30 +0200

    all pages created/addet and linked to navbar

--- Bournonvilles.7.3.5/Scripts/navBar.js
+++ Bournonvilles.7.3.5/Scripts/navBar.js
@@ -1,5 +1,5 @@
 ﻿$(document).ready(function () {
-    $('.dropdown-submenu>a').unbind('click').click(function (e) {
+    $('.dropdown-submenu>a').unbind('mouseover').mouseover(function (e) {
         $(this).next('ul').toggle();
         e.stopPropagation();
         e.preventDefault();
