8fb46718f7de53b09d999b24ed6ac84ae222f42a
massidk <e-david@live.dk>
2016-05-10 11:53:38 +0200

    redesign of navbar and footer

--- Bournonvilles.7.3.5/Scripts/cbpAnimatedHeader.js
+++ Bournonvilles.7.3.5/Scripts/cbpAnimatedHeader.js
@@ -13,7 +13,7 @@ var cbpAnimatedHeader = (function() {
 	var docElem = document.documentElement,
 		header = document.querySelector( '.cbp-af-header' ),
 		didScroll = false,
-		changeHeaderOn = 300;
+		changeHeaderOn = 150;
 
 	function init() {
 		scrollPage();
--- Bournonvilles.7.3.5/Scripts/navBar.js
+++ Bournonvilles.7.3.5/Scripts/navBar.js
@@ -1,8 +1,8 @@
-﻿$(document).ready(function () {
-    $('.dropdown-submenu>a').unbind('mouseover').mouseover(function (e) {
-        $(this).next('ul').toggle();
-        e.stopPropagation();
-        e.preventDefault();
-    });
-});
+﻿//$(document).ready(function () {
+//    $('.dropdown-submenu>a').unbind('mouseover').mouseover(function (e) {
+//        $(this).next('ul').toggle();
+//        e.stopPropagation();
+//        e.preventDefault();
+//    });
+//});
 
